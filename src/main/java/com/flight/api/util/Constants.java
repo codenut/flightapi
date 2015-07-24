3c8f2c997fe28ffb0c5fad0f6babf1be0ff96738
Michael Valladolid <mikevalladolid@gmail.com>
2015-07-24 15:11:20 +0800

    change error message

--- src/main/java/com/flight/api/util/Constants.java
+++ src/main/java/com/flight/api/util/Constants.java
@@ -4,5 +4,5 @@ package com.flight.api.util;
  * Created by michael on 7/23/15.
  */
 public class Constants {
-    public static final String ROUTE_NOT_FOUND = "Route not found.";
+    public static final String ROUTE_NOT_FOUND = "No airline operating between desired locations";
 }
