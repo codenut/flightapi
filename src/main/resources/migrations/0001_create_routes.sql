e9fa6dfda600732986c43bd6a664d85513104d77
Michael Valladolid <mikevalladolid@gmail.com>
2015-08-05 14:38:08 +0800

    fix sql create statements

--- src/main/resources/migrations/0001_create_routes.sql
+++ src/main/resources/migrations/0001_create_routes.sql
@@ -1,12 +1,12 @@
 --liquibase formatted sql
 
 --changeset mval:0001_create_routes
-CREATE TABLE `routes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `origin` varchar(64) DEFAULT NULL,
-  `destination` varchar(64) DEFAULT NULL,
-  `airline` varchar(128) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE routes (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  origin varchar(64) DEFAULT NULL,
+  destination varchar(64) DEFAULT NULL,
+  airline varchar(128) DEFAULT NULL,
+  PRIMARY KEY (id)
 );
 
 
